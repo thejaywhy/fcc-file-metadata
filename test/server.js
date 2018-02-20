68511882b3c72ceb011686feffee3ccb08aba8ac
Jon Young <jyoung@dyn.com>
2018-02-20 09:03:42 -0500

    add tests that cover both user stories

--- test/server.js
+++ test/server.js
@@ -7,6 +7,10 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var should = chai.should();
 
+var fs = require('fs')
+
+var TEST_FILE = "server.js";
+
 chai.use(chaiHttp);
 
 describe('App', () => {
@@ -21,11 +25,45 @@ describe('App', () => {
         .get('/')
         .end((err, res) => {
           res.should.have.status(200);
-          res.should.have.header('content-type', 'text/html; charset=UTF-8');
+          res.should.be.html;
 
           done();
         });
     })
   });
+
+  describe('/GET api/files', () => {
+    it('it should return a 404', (done) => {
+      chai.request(server)
+        .get('/api/files')
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.should.be.html;
+
+          done();
+        });
+    })
+  });
+
+  describe('/POST api/files', () => {
+    it('it should return a 200 and file size', (done) => {
+      testSize = fs.statSync(TEST_FILE).size;
+
+      chai.request(server)
+        .post('/api/files')
+        .attach('file', fs.readFileSync(TEST_FILE), 'server.js')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('size');
+          res.body.size.should.equal(testSize);
+
+          done();
+        });
+    })
+  });
+
+
 
 });
